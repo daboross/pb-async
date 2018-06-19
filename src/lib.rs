5d87a48d0db38ee01787bd5a74a94deee2319d9c
David Ross <daboross@daboross.net>
2018-06-19 04:13:38 -0700

    Release pb-async 0.1.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 #![warn(missing_docs)]
-#![doc(html_root_url = "https://docs.rs/pb-async/0.0.1/")]
+#![doc(html_root_url = "https://docs.rs/pb-async/0.1.0/")]
 //! Asynchronous PushBullet client for Rust.
 //!
 //! # Usage
